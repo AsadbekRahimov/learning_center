313f66bf5d16a18769fdacdbc58708423490ebbc
Asadbek <asadbek.rahimov1006@gmail.com>
2022-05-30 10:39:17 +0500

    customized header and footer

---
+++ resources/views/vendor/platform/footer.blade.php
@@ -0,0 +1,13 @@
+@guest
+    <p><a href="https://t.me/Asadbek_Rahimov">Developed by Asadbek Rahimov</a></p>
+@else
+
+    <div class="text-center user-select-none">
+        <p class="small m-0">
+            Saxovat ta'lim o'quv markazi.<br>
+            <a href="https://t.me/Asadbek_Rahimov" target="_blank" rel="noopener">
+                Developed by Asadbek Rahimov
+            </a>
+        </p>
+    </div>
+@endguest
