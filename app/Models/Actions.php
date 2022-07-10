9aeffb2bca28fac7e0d9ce7e9a246e7a46a4c982
Asadbek <asadbek.rahimov1006@gmail.com>
2022-07-10 19:28:40 +0500

    add discounts and actions table and models for this tables

---
+++ app/Models/Actions.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Orchid\Attachment\Attachable;
+use Orchid\Filters\Filterable;
+use Orchid\Screen\AsSource;
+
+class Actions extends Model
+{
+    use HasFactory;
+    use AsSource, Filterable, Attachable;
+
+    protected $fillable = [
+        'student_id',
+        'price',
+        'type',
+        'action',
+        'desc',
+    ];
+
+    public const TYPES = [
+        'test' => 'test',
+    ];
+}
