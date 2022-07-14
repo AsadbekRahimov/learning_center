79cf6560ebcc353885ec2d9508ed987aa89ac5a3
AsadbekRahimov <asadbek.rahimov1006@gmail.com>
2022-07-14 17:24:48 +0500

    add change student status action

--- app/Models/Action.php
+++ app/Models/Action.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
 use Orchid\Attachment\Attachable;
 use Orchid\Filters\Filterable;
 use Orchid\Screen\AsSource;
@@ -22,6 +23,15 @@ class Action extends Model
     ];
 
     public const TYPES = [
-        'test' => 'test',
+        'changeStatus' => 'Talaba statusi yangilandi',
     ];
+
+    public static function changeStudentStatus(Student $student,  $status)
+    {
+        return self::query()->create([
+            'student_id' => $student->id,
+            'type' => 'changeStatus',
+            'desc' => 'Talaba statusi ' . Student::STATUS[$student->status] . ' dan ' . Student::STATUS[$status] . ' ga ' . Auth::user()->name . ' tomonidan yangilandi!',
+        ]);
+    }
 }
